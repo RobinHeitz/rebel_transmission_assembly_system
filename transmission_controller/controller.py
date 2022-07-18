19b60144b2ad963db817ba6c924c8f7c20b72c66
Robin Heitz <robin_rob96@me.com>
2022-07-18 10:40:15 +0200

    small changes

--- transmission_controller/controller.py
+++ transmission_controller/controller.py
@@ -18,6 +18,20 @@ from calculations import bytes_to_int, int_to_bytes
 from definitions import RESPONSE_ERROR_CODES, RESPONSE_ERROR_CODES_DICT
 
 
+import logging
+logFormatter = logging.Formatter("'%(asctime)s - %(message)s")
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+fileHandler = logging.FileHandler("log.log", mode="w")
+fileHandler.setFormatter(logFormatter)
+logger.addHandler(fileHandler)
+
+consolerHandler = logging.StreamHandler()
+consolerHandler.setFormatter(logFormatter)
+logger.addHandler(consolerHandler)
+
+
 can_id = 0x20 #CAN ID???
 std_baudrate = PCAN_BAUD_500K
 
@@ -38,15 +52,15 @@ def validate_all_channels():
         pcan = PCANBasic()
         status = pcan.Initialize(c, std_baudrate)
         if status != PCAN_ERROR_OK:
-            print(f"Init of pcan channel PCAN_USBBUS{index} has failed.")
+            logger.debug(f"Init of pcan channel PCAN_USBBUS{index} has failed.")
         else:
-            print(f"Init channel PCAN_USBBUS{index} was successful.")
+            logger.debug(f"Init channel PCAN_USBBUS{index} was successful.")
 
         pcan.Uninitialize(c)
 
-def get_status_description(pcan, channel):
-    status, message, timestamp_ = pcan.Read(channel)
-    return _status_str(status)
+# def get_status_description(pcan, channel):
+#     status, message, timestamp_ = pcan.Read(channel)
+#     return _status_str(status)
 
 
 def get_status(pcan, channel):
@@ -56,11 +70,12 @@ def get_status(pcan, channel):
     return _status_str(status), message, timestamp
     
 
-def _get_cmd_msg(data, id=can_id):
+def _get_cmd_msg(data, data_len = 8, id=can_id):
     """Basic construction of a can message with 8 bytes.
     
     Params:
-    data: List of bytes as hex."""
+    data: List of bytes as hex.
+    data_len: Length of data in bytes"""
     msg = TPCANMsg()
     msg.DATA = (c_ubyte * 8)()
 
@@ -82,17 +97,34 @@ def _write_cmd(pcan, channel, msg, cmd_description=""):
 
 
 def cmd_reset_errors(pcan, channel):
-    msg = _get_cmd_msg([0x01, 0x06])
+    logger.debug("cmd_reset_errors()")
+    msg = _get_cmd_msg([0x01, 0x06], data_len=2)
     _write_cmd(pcan, channel, msg, "reset_errors")
 
 
 def cmd_enable_motor(pcan, channel):
+    logger.debug("cmd_enable_motor()")
     msg = _get_cmd_msg([0x01, 0x09])
     _write_cmd(pcan, channel, msg, "enable_motors")
 
+
+def cmd_reset_position(pcan, channel):
+    logger.debug("cmd_reset_position()")
+    """Needs to be sent 2 times within 20ms at start."""
+    msg = _get_cmd_msg([0x01, 0x08], data_len=4)
+    _write_cmd(pcan, channel, msg, "reset_position")
+
+
 def cmd_disable_motor(pcan, channel):
+    logger.debug("cmd_disable_motor()")
     msg = _get_cmd_msg([0x01, 0x0A])
     _write_cmd(pcan, channel, msg, "disable_motors")
+
+
+def cmd_allign_rotor(pcan, channel):
+    logger.debug("cmd_allign_rotor()")
+    msg = _get_cmd_msg([0x01, 0x0C])
+    _write_cmd(pcan, channel, msg, "align_rotor")
 
 
 def cmd_velocity_mode(pcan, channel, velo=10):
@@ -101,6 +133,10 @@ def cmd_velocity_mode(pcan, channel, velo=10):
     pcan: PCANBasic instance
     channel: Current PCANChannel
     velo: Velocity transmission-sided in [°/sec]"""
+    
+    logger.debug("#"*10)
+    logger.debug("cmd_velocity_mode()")
+    
     if abs(velo) > 36:
         raise TypeError("Parameter velo should not be greater than 36°/sec!")
 
@@ -128,7 +164,7 @@ def _timestamp_str(timestamp):
 def _status_str(status):
     return f"Current Status: {PCAN_DICT_STATUS.get(status)}"
 
-def __response_error_codes(error_byte):
+def _response_error_codes(error_byte):
     #check if the 1st bit is in set:
     errors = []
     for i in range(0,8):
@@ -145,40 +181,41 @@ def read_messages(pcan, channel):
     Reads a message. The answer from a movement command has the CAN-ID = BoardID + 1
     
     """
+    logger.debug("#"*10)
+    logger.debug("read_messages()")
     status, msg, timestamp = pcan.Read(channel)
 
     if msg.ID == can_id + 1:
-        # received answer to movement cmds
-        
+        # answer to movement cmds
+        logger.debug("Received msg CAN-ID + 1")
         data = msg.DATA
         
-        error_byte = data[0] #Fehler byte
-
-        print("Error Byte = ", error_byte)
-        error_codes = __response_error_codes(error_byte)
-        print("#"*30)
-        print(error_codes)
-
-        
-        #32 bit position [Tics]
-        pos1, pos2, pos3, pos4 = data[1],data[2],data[3],data[4]
+        error_codes = _response_error_codes(data[0])
+        logging.info(f"Error codes while receiving msg CAN-ID + 1: {error_codes}")
 
         # tics from motor encoder
         tics = bytes_to_int(data[1:5],signed=True)        
         
         # pos transission [°]
         pos = tics / 1031.111
-        print(f"Current Position: {pos} // Tics: {tics}")
+        logger.info(f"Current Position: {pos} // Tics: {tics}")
 
         current = bytes_to_int(data[5:7], signed=True)
-        print(f"Current: {current}")
-
-
-        
-
+        logger.info(f"Current: {current}")
+    
+   
+    elif msg.ID == can_id + 2:
+        # answer to reset error (0x06) cmd / reset position cmd (0x09) / Gear output encoder (Abtriebsencoder/ Absolutwertgeber)
+        ...
+        logger.debug("Received msg CAN-ID + 2")
+    
+    elif msg.ID == can_id + 3:
+        # once a second the controller sends a message voltage, motor temperature and board temperature
+        logger.debug("Received msg CAN-ID + 3")
+        ...
+   
     else:
-        pass
-        print("Different Msg ID (int):", msg.ID, "and in hex: ", hex(msg.ID))
+        logger.debug("Different Msg ID (int):", msg.ID, "and in hex: ", hex(msg.ID))
     
 
     
@@ -190,7 +227,7 @@ if __name__ == "__main__":
 
         validate_all_channels()
 
-        print("STARTING NOW")
+        logger.info("STARTING NOW")
 
         current_channel = PCAN_USBBUS1
 
@@ -200,13 +237,12 @@ if __name__ == "__main__":
         if status != PCAN_ERROR_OK:
             raise Exception(f"Initializing failed! Status = {_status_str(status)}")
         else:
-            print("Connection was succesfull")
+            logger.info("Connection was succesfull")
 
 
-        print("Current status is: ", get_status_description(pcan, current_channel))
-
-        status, message, timestamp = get_status(pcan, current_channel)
-        print(f"MessageID: {message.ID}, message-type: {message.MSGTYPE}, message length: {message.LEN}")
+        cmd_reset_position(pcan, current_channel)
+        time.sleep(1/100)
+        cmd_reset_position(pcan, current_channel)
         
         cmd_reset_errors(pcan, current_channel)
         read_messages(pcan, current_channel)
