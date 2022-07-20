ad06a5c32043d55c7f0b64e006d07c60fd32811f
Robin Heitz <robin_rob96@me.com>
2022-07-20 09:12:44 +0200

    function calls now mapped in dict

--- start_app.py
+++ start_app.py
@@ -2,6 +2,12 @@ import PySimpleGUI as sg
 
 from hw_interface.motor_controller import RebelAxisController
 
+import threading
+
+import time
+
+
+
 
 L_PAGE_1 = "-LAYOUT_PAGE_1-"
 L_PAGE_2 = "-LAYOUT_PAGE_2-"
@@ -10,24 +16,27 @@ F_RADIO_BUTTON_80_CLICKED = "-FUNCTION_RADIO_BUTTON_80_CLICKED-"
 F_RADIO_BUTTON_105_CLICKED = "-FUNCTION_RADIO_BUTTON_105_CLICKED-"
 F_BTN_CONNECT_CAN = "-KEY_BUTTON_CONNECT_CAN-"
 F_BTN_NEXT_PAGE = "-KEY_BUTTON_NEXT_PAGE-"
+F_BTN_TEST = "-TEST-"
+
+E_LONG_OPERATION_UPDATE = "-LONG_OPERATION_UPDATE-"
+E_LONG_OPERATION_DONE = "-LONG_OPERATION_DONE-"
 
 
-
-def radio_80_clicked():
+def radio_80_clicked(event, values):
     checkbox = window[("checkbox", "break_existing")]
     checkbox.update(disabled=True)
     # checkbox.hide_row()
 
-def radio_105_clicked():
+def radio_105_clicked(event, values):
     checkbox = window[("checkbox", "break_existing")]
     checkbox.update(disabled=False)
     # checkbox.unhide_row()
 
-def connect_can(controller):
+def connect_can(event, values,controller):
     print("Connect can")
     controller.connect()
 
-def next_page():
+def next_page(event, values):
     print("next page")
     prev_layout = window[L_PAGE_1]
     prev_layout.update(visible=False)
@@ -35,6 +44,23 @@ def next_page():
 
     next_layout = window[L_PAGE_2]
     next_layout.update(visible=True)
+
+
+def long_operation(event, values, window):
+    ...
+    threading.Thread(target=long_operation_thread, args=(event, values, window,), daemon=True).start()
+
+
+
+def long_operation_thread(event, values, window):
+    for i in range(10):
+        time.sleep(2)
+        print(f"long operation - {i}")
+        window.write_event_value(E_LONG_OPERATION_UPDATE, i)
+    window.write_event_value(E_LONG_OPERATION_DONE, i)
+
+
+
 
 
 
@@ -67,7 +93,7 @@ layout_page_1 = [
     ]
 
 layout_page_2 = [
-    [sg.Text("Seite2!"), sg.Button("Move Motor", key="-TEST-", enable_events=True)]
+    [sg.Text("Seite2!"), sg.Button("Move Motor", key=F_BTN_TEST, enable_events=True)]
 
 ]
 
@@ -77,32 +103,73 @@ layout = [
     [sg.Column(layout_page_2, visible=False, key=L_PAGE_2),],
 ]
 
+
+def my_test_func(event, values):
+    long_operation(window)
+
+def handle_update(event, values):
+    print("update long event: ", event, values.get(E_LONG_OPERATION_UPDATE))
+
+def handle_finish(event, values):
+    print("MULTITASKING FINISHED", event, values.get(E_LONG_OPERATION_DONE))
+
 if __name__ == "__main__":
     window = sg.Window("ReBeL Getriebe Montage & Kalibrierung", layout, size=(800,500))
-
     controller = RebelAxisController()
 
+    key_function_map = {
+        F_RADIO_BUTTON_80_CLICKED: (radio_80_clicked, dict()),
+        F_RADIO_BUTTON_105_CLICKED:( radio_105_clicked, dict()),
+
+        F_BTN_CONNECT_CAN: (connect_can, dict(controller=controller)),
+        F_BTN_NEXT_PAGE: (next_page, dict()),
+        F_BTN_TEST:  (long_operation, dict(window=window)), 
+
+        E_LONG_OPERATION_UPDATE: (handle_update, dict()),
+        E_LONG_OPERATION_DONE: (handle_finish, dict()),
+
+
+
+    }
     # Event loop
     while True:
         event, values = window.read()
         if event in (sg.WIN_CLOSED, 'Exit'):
             break
-        
-        elif event == F_RADIO_BUTTON_80_CLICKED:
-            radio_80_clicked()
-        
-        elif event == F_RADIO_BUTTON_105_CLICKED:
-            radio_105_clicked()
-        
-        elif event == F_BTN_CONNECT_CAN:
-            connect_can(controller)
 
-        elif event  == F_BTN_NEXT_PAGE:
-            next_page()
-
-        elif event == "-TEST-":
-            controller.movement_velocity_mode()
         
+        func, args = key_function_map.get(event)
+        if func is not None and args is not None:
+            func(event, values, **args)
+        # if func is not None and args is not None:
+        #     func(**args)
+
+
+
+        # elif event == F_RADIO_BUTTON_80_CLICKED:
+        #     radio_80_clicked()
+        
+        # elif event == F_RADIO_BUTTON_105_CLICKED:
+        #     radio_105_clicked()
+        
+        # elif event == F_BTN_CONNECT_CAN:
+        #     connect_can(controller)
+
+        # elif event  == F_BTN_NEXT_PAGE:
+        #     next_page()
+
+        # elif event == F_BTN_TEST:
+        #     # controller.movement_velocity_mode()
+        #     # window.perform_long_operation(long_operation, "-END_OPERATION-")
+            
+        #     long_operation(window)
+        
+        # elif event == E_LONG_OPERATION_UPDATE:
+        #     print("update long event: ", event, values.get(E_LONG_OPERATION_UPDATE))
+
+
+        # elif event == E_LONG_OPERATION_DONE:
+        #     print("MULTITASKING FINISHED", event, values.get(E_LONG_OPERATION_DONE))
     
 
 
