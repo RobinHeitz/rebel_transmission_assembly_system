1717c5147a58c7070f2c41efcc429594352a7285
Robin Heitz <robin_rob96@me.com>
2022-08-25 11:02:28 +0200

    modified db_delete_all script

--- db_delete_all.py
+++ db_delete_all.py
@@ -2,8 +2,8 @@
 
 import sqlalchemy as db
 
-from data_management.model import Transmission, TransmissionConfiguration
-from data_management.model import Measurement, Assembly, AssemblyStep, DataPoint
+from data_management.model import Failure, Transmission, TransmissionConfiguration, AssemblyStep
+from data_management.model import Measurement, Assembly, AssemblyStep, DataPoint, FailureType
 
 
 from sqlalchemy.orm import sessionmaker
@@ -14,12 +14,24 @@ if __name__ == "__main__":
     metadata = db.MetaData()
     session = sessionmaker(bind = engine)()
 
-    model_classes = [Transmission, Assembly, Measurement, DataPoint]
+    model_classes = [Transmission, Assembly, Measurement, DataPoint, FailureType, Failure]
 
     objects_to_delete = [i for c in model_classes for i in session.query(c).all()]
     for i in objects_to_delete:
         session.delete(i)
     
     session.flush()
+
+
+    for step in AssemblyStep:
+        
+        overcurrent = FailureType(description = f"Current to high for this assembly step! {step.value}", assembly_step = step)
+        another_failure_type = FailureType(description = f"This is a failure, special to this assembly step: {step.value}", assembly_step = step)
+        
+        session.add(overcurrent)
+        session.add(another_failure_type)
+
     session.commit()
+
+
 
