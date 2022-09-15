2ad4d09b6f90b1e7c1dca639dc58537b742c1e55
Robin Heitz <robin_rob96@me.com>
2022-09-15 14:38:50 +0200

    Bug fixed

--- gui/plotting.py
+++ gui/plotting.py
@@ -21,38 +21,54 @@ class GraphPlotter:
         self.ax = self.figure.add_subplot(111)
         self.ax.set_autoscaley_on(True)
 
+    def __plot(self, data_x, data_y, limit):
+        self.logger.info(f"_plot()| limit: {limit}")
+        self.line.set_xdata(data_x)
+        self.line.set_ydata(data_y)
+        # self.line.set_label("Label Test")
+        # self.ax.legend()
+
+        if limit != None and self.limit_line == None:
+            if self.limit_line == None:
+                self.limit_line = self.ax.axhline(y=limit, color="red", linestyle="dashed")
+                self.logger.info(f"Numb of lines: {self.ax.lines}")
+
+        elif limit == None and self.limit_line != None:
+            self.limit_line.remove()
+            del self.limit_line
+
+
+
+
+
+        # self.logger.debug
+        # if self.limit_line != None:
+        #     self.logger.debug("self.limit_line != None")
+
+        #     self.limit_line.remove()
+        
+        # if limit != None:
+        #     self.logger.debug("self.limit != None")
+        #     self.limit_line, _ = self.ax.axhline(y = limit, color="red", linestyle = "dashed")
+        
+        self.ax.set_title("Stromaufnahme eines Bewegungsablaufs")
+        self.ax.set_ylabel("Strom [mA]")
+        self.ax.set_xlabel("Zeitstempel")
+        
+        self.ax.relim()
+        self.ax.autoscale_view()
+        self.figure.canvas.draw()
+        self.figure.canvas.flush_events()
 
     def plot_data(self, data_x, data_y, limit = None):
-        self.logger.info("*"*10)
-        self.logger.info(f"plot_data():{limit}")
-        self.logger.info("*"*10)
-        
-        def _plot(data_x, data_y):
-            self.line.set_xdata(data_x)
-            self.line.set_ydata(data_y)
-            # self.line.set_label("Label Test")
-            # self.ax.legend()
-            
-            self.ax.set_title("Stromaufnahme eines Bewegungsablaufs")
-            self.ax.set_ylabel("Strom [mA]")
-            self.ax.set_xlabel("Zeitstempel")
-            
-            self.ax.relim()
-            self.ax.autoscale_view()
-            self.figure.canvas.draw()
-            self.figure.canvas.flush_events()
-
-
-        if limit != None:
-            self.limit_line = self.ax.axhline(y = limit, color="red", linestyle = "dashed")
-
+        self.logger.info(f"plot_data; limit = {limit}")
 
         if not self.line:
             self.line, = self.ax.plot(data_x, data_y, "r-", color="blue")
-            _plot(data_x, data_y)
+            self.__plot(data_x, data_y, limit)
             self.__draw_figure_in_canvas()
         else:
-            _plot(data_x, data_y)
+            self.__plot(data_x, data_y, limit)
         
     
     def __draw_figure_in_canvas(self):
