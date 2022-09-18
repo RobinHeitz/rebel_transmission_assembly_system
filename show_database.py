72c156135836677f6fc4f79979f52d170eef1586
Robin Heitz <robin_rob96@me.com>
2022-09-18 09:32:42 +0200

    some database showcases

--- show_database.py
+++ show_database.py
@@ -31,7 +31,7 @@ def list_fails(assembly_step: int):
     table = Table(title=f"Failures for assembly step {step.name}")
     table.add_column("id", justify="left", style="cyan", no_wrap=True)
     table.add_column("description", justify="left", style="cyan")
-    table.add_column("Num instances", justify="right", style="green")
+    table.add_column("# instances", justify="right", style="green")
 
 
 
@@ -58,10 +58,10 @@ def list_imps(assembly_step: int):
     
     table = Table(title=f"Improvements for assembly step {step.name}")
     table.add_column("id", justify="left", style="cyan", no_wrap=True)
-    table.add_column("title", justify="left", style="cyan")
+    table.add_column("title", justify="left", style="cyan", max_width=15)
     table.add_column("description", justify="left", style="cyan")
-    table.add_column("Cable disconnect", justify="right", style="green")
-    table.add_column("Num instances", justify="right", style="green")
+    table.add_column("Cable disconnect", justify="right", style="green", max_width=5)
+    table.add_column("# instances", justify="right", style="green", max_width=5)
 
 
     for i in improvements:
@@ -72,6 +72,37 @@ def list_imps(assembly_step: int):
     console = Console()
     console.print(table)
     session.close()
+
+
+@app.command()
+def list_imp_instances(step: int):
+    with data_controller.session_context() as session:
+        try:
+            step = AssemblyStep(step)
+        except ValueError:
+            print(f"[bold red]There is no assembly step with value = {step}![/bold red]")
+            return
+
+        imps = session.query(Improvement).filter_by(assembly_step = step).all()
+
+        imp_instances = {instance for imp in imps for instance in session.query(ImprovementInstance).filter_by(improvement = imp).all() }
+
+        print(f"instances found: {len(imp_instances)}")
+        
+        
+        # table = Table(title=f"FailureInstance for failure: {fail}")
+        # table.add_column("id", justify="left", style="cyan", no_wrap=True)
+        # table.add_column("AssemblyStep", justify="left", style="cyan")
+        # table.add_column("Measurement-id", justify="right", style="green", max_width=5)
+
+        # for fi in fail_instances:
+        #     table.add_row(str(fi.id), str(fi.assembly_step.name), str(fi.measurement.id))
+
+        # console = Console()
+        # console.print(table)
+        # session.close()
+    
+
 
 
 
