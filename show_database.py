a82b46dd05a7eaf76ea423a5a20a7edaf1142194
Robin Heitz <robin_rob96@me.com>
2022-09-21 09:52:28 +0200

    verify failures implemented

--- show_database.py
+++ show_database.py
@@ -1,6 +1,6 @@
 from msilib.schema import Error
 from data_management import data_controller
-from data_management.model import AssemblyStep, Failure, FailureInstance, Improvement, ImprovementInstance
+from data_management.model import AssemblyStep, Failure, FailureInstance, FailureType, Improvement, ImprovementInstance
 from rich import print
 from rich.table import Table
 from rich.console import Console
@@ -12,9 +12,10 @@ import typer
 
 app = typer.Typer()
 console = Console()
+session = data_controller.create_session()
 
 def list_assembly_steps() -> Table:
-    """Lists all assembly steps in a table and returns it then."""
+    """Lists all assembly steps in a table and prints it to the console."""
     table = Table(title="Assembly Steps")
     table.add_column("id", justify="center", style="magenta")
     table.add_column("name", justify="left", style="cyan")
@@ -24,14 +25,13 @@ def list_assembly_steps() -> Table:
     console.print(table)
 
 def list_failures(failures:List[Failure]) -> None:
-    """Lists failures in a table and returns it then."""
+    """Lists failures in a table and prints it to the console."""
     
     table = Table(title=f"Failures")
     table.add_column("id", justify="left", style="cyan", no_wrap=True)
     table.add_column("verified", justify="center", style="red")
     table.add_column("created at", justify="left", style="green")
     table.add_column("description", justify="left", style="cyan")
-    table.add_column("failure type", justify="left", style="green")
     table.add_column("# Improvements", justify="center", style="magenta")
 
     for f in failures:
@@ -40,124 +40,84 @@ def list_failures(failures:List[Failure]) -> None:
             str(f.is_verified), 
             f.created_at.strftime('%d. %b %y, %H:%M'),
             str(f.description), 
-            str(f.failure_type.name), 
             str(len(f.improvements)), 
         )
     console.print(table)
 
 
+
 def input_assembly_step() -> AssemblyStep:
-    table = list_assembly_steps()
-    id = console.input("Which assembly step do you want to inspect? :smiley: Input id of desired step.")
+    id = console.input("Which assembly step do you want to inspect? :smiley: Input id of desired step.\n")
 
     while True:
         try:
             step = AssemblyStep(int(id))
             break
         except Exception:
-            id = console.input("[red]Wrong input![/red] You need to input the id of desired assembly step!")
+            id = console.input("[red]Wrong input![/red] You need to input the id of desired assembly step!\n")
     return step
+
+
+def input_failure_detail_index(valid_indices: List[int]) -> Failure:
+    id = console.input("Which failure do you want to change the verified status? Input id of failure.\n")
+    while True:
+        try:
+            index_ = int(id)
+            if index_ in valid_indices:
+                return session.query(Failure).get(index_)
+            else:
+                raise Exception("Wront input!")
+
+        except:
+            id = console.input("[red]Wrong input![/red] You need to input the id of desired failure!\n")
+
+def input_changed_verify_status() -> None:
+    """User inputs new status for given failure."""
+    id = console.input("To what verified status do you want to change the selected failure? Input 'True', 'False' or 'Cancel' to cancel the action.\n")
+    id = id.lower()
+
+    while True:
+        if id in ["true", "false", "cancel"]:
+            ...
+            break
+        else:
+            id = console.input("[red]Wrong input![/red] You need to input the the new value of <is verified>: 'True' or 'False'. Type 'Cancel' to abort!\n")
+
+    answers = dict(
+        true = True, 
+        false = False,
+    )
+    if id == "cancel":
+        exit()
+    else:
+        return answers[id]
 
 
 
 @app.command()
 def verify_failures():
-    """Verifies created failures that are not yet accessable for others."""
+    """Change is_verified - status of failures. If not verified (yet), failures are not displayed in the programm."""
+    list_assembly_steps()
     step = input_assembly_step()
+  
     session = data_controller.create_session()
-    failures = session.query(Failure).filter_by(assembly_step = step).all()
+    failures = session.query(Failure).filter_by(assembly_step = step, failure_type = FailureType.not_measurable).all()
     list_failures(failures)
 
-
-
-# @app.command()
-# def list_fails(assembly_step: int):
-#     session = data_controller.create_session()
+    fail_indices = [f.id for f in failures]
+    fail = input_failure_detail_index(fail_indices)
+    new_verified = input_changed_verify_status()
     
-#     try:
-#         step = AssemblyStep(assembly_step)
-#     except ValueError:
-#         ...
+    fail.is_verified = new_verified
+    session.commit()
+    print("[magenta]Changes are saved. Current database status:[/magenta]")
+    list_failures(failures)
 
-#     print(f"Hello, selected AssemblyStep: [bold magenta]{step.name}[/bold magenta]!")
-
-#     failures = session.query(Failure).filter_by(assembly_step = step).all()
-
-
-#     table = Table(title=f"Failures for assembly step {step.name}")
-#     table.add_column("id", justify="left", style="cyan", no_wrap=True)
-#     table.add_column("description", justify="left", style="cyan")
-#     table.add_column("# instances", justify="right", style="green")
-
-
-
-#     for f in failures:
-#         num_f_instances = session.query(FailureInstance).filter_by(assembly_step = step, failure = f).count()
-
-#         table.add_row(f"{f.id}", f.description, str(num_f_instances))
+@app.command()
+def verify_improvements():
+    """Change is_verified - status of improvements. If not verified (yet), improvements are not displayed in the programm."""
     
-#     console = Console()
-#     console.print(table)
-#     session.close()
 
-
-# @app.command()
-# def list_imps(assembly_step: int):
-#     session = data_controller.create_session()
-#     try:
-#         step = AssemblyStep(assembly_step)
-#     except ValueError:
-#         ...
-#     print(f"Hello, selected AssemblyStep: [bold magenta]{step.name}[/bold magenta]!")
-
-#     improvements = session.query(Improvement).filter_by(assembly_step = step).all()
-    
-#     table = Table(title=f"Improvements for assembly step {step.name}")
-#     table.add_column("id", justify="left", style="cyan", no_wrap=True)
-#     table.add_column("title", justify="left", style="cyan", max_width=15)
-#     table.add_column("description", justify="left", style="cyan")
-#     table.add_column("Cable disconnect", justify="right", style="green", max_width=5)
-#     table.add_column("# instances", justify="right", style="green", max_width=5)
-
-
-#     for i in improvements:
-#         num_instances = session.query(ImprovementInstance).filter_by(assembly_step = step, improvement = i).count()
-
-#         table.add_row(str(i.id),i.title, i.description, str(i.cable_must_disconnected), str(num_instances))
-    
-#     console = Console()
-#     console.print(table)
-#     session.close()
-
-
-# @app.command()
-# def list_imp_instances(step: int):
-#     with data_controller.session_context() as session:
-#         try:
-#             step = AssemblyStep(step)
-#         except ValueError:
-#             print(f"[bold red]There is no assembly step with value = {step}![/bold red]")
-#             return
-
-#         imps = session.query(Improvement).filter_by(assembly_step = step).all()
-
-#         imp_instances = {instance for imp in imps for instance in session.query(ImprovementInstance).filter_by(improvement = imp).all() }
-
-#         print(f"instances found: {len(imp_instances)}")
-        
-        
-#         # table = Table(title=f"FailureInstance for failure: {fail}")
-#         # table.add_column("id", justify="left", style="cyan", no_wrap=True)
-#         # table.add_column("AssemblyStep", justify="left", style="cyan")
-#         # table.add_column("Measurement-id", justify="right", style="green", max_width=5)
-
-#         # for fi in fail_instances:
-#         #     table.add_row(str(fi.id), str(fi.assembly_step.name), str(fi.measurement.id))
-
-#         # console = Console()
-#         # console.print(table)
-#         # session.close()
-    
 
 
 
